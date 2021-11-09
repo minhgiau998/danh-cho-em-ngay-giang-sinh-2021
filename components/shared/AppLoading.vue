0849d3492f2878466755f91750b8aff383a7a5b2
Rich Nguyen <minhgiau04041998@gmail.com>
2021-11-09 08:32:51 +0700

    add feature loading

---
+++ components/shared/AppLoading.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    v-if="loading"
+    class="fixed inset-0 flex items-center justify-center w-full h-full transition-opacity duration-300 bg-white "
+    style="z-index: 6000"
+  >
+    <div class="flex-col">
+      <img src="~/assets/images/gift.gif" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-loading',
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,37 @@
 <template>
-  <app-header></app-header>
+  <div>
+    <app-loading :loading="show"></app-loading>
+    <app-header></app-header>
+  </div>
 </template>
 
 <script>
+import AppLoading from '../components/shared/AppLoading.vue'
 import AppHeader from '../components/shared/AppHeader.vue'
 
 export default {
   components: {
+    AppLoading,
     AppHeader,
+  },
+  data() {
+    return {
+      show: false,
+      label: 'Loading...',
+      overlay: true,
+      timeOut: 2000,
+    }
+  },
+  mounted() {
+    this.showMe()
+  },
+  methods: {
+    showMe() {
+      this.show = true
+      setTimeout(() => {
+        this.show = false
+      }, this.timeOut)
+    },
   },
 }
 </script>
